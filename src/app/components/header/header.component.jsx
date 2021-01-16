c354b541fdae28eab109f2ceea1cebda6c557b92
MomohNobert <okhai0112@gmail.com>
2021-01-16 18:18:59 +0100

    changes to header component

--- src/app/components/header/header.component.jsx
+++ src/app/components/header/header.component.jsx
@@ -1,10 +1,12 @@
 import Logo from "../../molecules/Logo/Logo.molecule";
+import Search from "../../molecules/Search/search.molecule";
 import { HeaderContainer } from "./header.styles";
 
 const Header = () => {
     return (
         <HeaderContainer>
             <Logo />
+            <Search />
         </HeaderContainer>
     )
 }
--- src/app/components/header/header.styles.jsx
+++ src/app/components/header/header.styles.jsx
@@ -4,4 +4,5 @@ export const HeaderContainer = styled.div`
     width: 100%;
     height: 12.5vh;
     box-shadow: 0px 0px 1px rgba(0,0,0,0.5);
+    display: flex;
 `;

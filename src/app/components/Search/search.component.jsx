8a3853e053e736f3ba61b642c4c26b4c7d241fb3
MomohNobert <okhai0112@gmail.com>
2021-01-16 22:18:40 +0100

    added functionality to search component

--- src/app/components/Search/search.component.jsx
+++ src/app/components/Search/search.component.jsx
@@ -1,15 +1,35 @@
 import { useState } from 'react';
 import Input from '../../molecules/Input/Input.molecule';
-import { SearchContainer } from './search.styles';
+import { SearchContainer, SearchIconContainer } from './search.styles';
+import { useDispatch } from 'react-redux';
+import { fetchMovies } from './search.slice';
+import SearchImg from './assets/search.svg';
 
 
 const Search = () => {
     const [searchValue, setSearchValue] = useState(null);
-    console.log(searchValue)
+    console.log(searchValue);
+
+    const dispatch = useDispatch();
+    // const { movies, loading, hasErrors } = useSelector(moviesSelector);
+
+    const clickHandler = value => {
+        console.log(value);
+        if (value) {
+            dispatch(fetchMovies(value))
+        }
+    }
 
     return (
         <SearchContainer>
-            <Input setSearchValue={setSearchValue} />
+            <Input 
+                setSearchValue={setSearchValue}
+            />
+            <SearchIconContainer
+                onClick={() => clickHandler(searchValue)}
+            >
+                <img src={SearchImg} alt="search" />
+            </SearchIconContainer>
         </SearchContainer>
     )
 }

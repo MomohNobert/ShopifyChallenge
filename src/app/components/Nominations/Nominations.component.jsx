26e80448946b1b552b4bcbbd14f3e520a3356f1e
MomohNobert <okhai0112@gmail.com>
2021-01-16 23:03:31 +0100

    created Nominations component and styling

---
+++ src/app/components/Nominations/Nominations.component.jsx
@@ -0,0 +1,11 @@
+import { NomContainer } from "./Nominations.styles"
+
+const Nominations = () => {
+    return (
+        <NomContainer>
+            Nominations
+        </NomContainer>
+    )
+}
+
+export default Nominations;
---
+++ src/app/components/Nominations/Nominations.styles.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const NomContainer = styled.div`
+    width: 25%;
+    height: 70vh;
+    background-color: white;
+    border-radius: 10px;
+    box-shadow: 0px 1px 5px rgba(0,0,0,0.1);
+`

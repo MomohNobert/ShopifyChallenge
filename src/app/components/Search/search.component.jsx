47fffdce58bd8c42d47d87dac55d7960c1b326c8
MomohNobert <okhai0112@gmail.com>
2021-01-16 18:33:05 +0100

    added input to search component

--- src/app/components/Search/search.component.jsx
+++ src/app/components/Search/search.component.jsx
@@ -1,9 +1,11 @@
+import Input from '../../molecules/Input/Input.molecule';
 import { SearchContainer } from './search.styles';
+
 
 const Search = () => {
     return (
         <SearchContainer>
-            Search
+            <Input />
         </SearchContainer>
     )
 }

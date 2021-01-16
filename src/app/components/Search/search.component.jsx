a74a093048136e88a056d35d9091cc2e7605a30b
MomohNobert <okhai0112@gmail.com>
2021-01-16 19:01:02 +0100

    added search value

--- src/app/components/Search/search.component.jsx
+++ src/app/components/Search/search.component.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import Input from '../../molecules/Input/Input.molecule';
 import { SearchContainer } from './search.styles';
 
 
 const Search = () => {
+    const [searchValue, setSearchValue] = useState(null);
+    console.log(searchValue)
+
     return (
         <SearchContainer>
-            <Input />
+            <Input setSearchValue={setSearchValue} />
         </SearchContainer>
     )
 }

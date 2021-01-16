bfd0cb9d85810185082c15000704c8aff38be8b3
MomohNobert <okhai0112@gmail.com>
2021-01-16 23:41:23 +0100

    restructuring movie template

--- src/app/templates/Movies/Movies.template.jsx
+++ src/app/templates/Movies/Movies.template.jsx
@@ -1,13 +1,22 @@
 import { MoviesContainer } from "./Movies.styles";
 import { useSelector } from 'react-redux';
+import EmptyMovies from "../../components/EmptyMovie/emptyMovie.component";
 
 const Movies = () => {
     const { loading, hasErrors, movies, searchValue } = useSelector(state => state.movies);
     console.log(loading, hasErrors, movies, searchValue);
 
+    const currentComponent = () => {
+        if (!searchValue) {
+            return <EmptyMovies />
+        } else {
+            return null
+        }
+    }
+
     return (
         <MoviesContainer>
-            Movies
+            {currentComponent()}
         </MoviesContainer>
     )
 }

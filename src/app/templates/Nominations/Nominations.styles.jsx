bf47cb4677ba93a1697b6b30c5ab3a623cc387d5
MomohNobert <okhai0112@gmail.com>
2021-01-17 01:39:51 +0100

    adjustments to nomination styling

--- src/app/templates/Nominations/Nominations.styles.jsx
+++ src/app/templates/Nominations/Nominations.styles.jsx
@@ -6,4 +6,7 @@ export const NomContainer = styled.div`
     background-color: white;
     border-radius: 10px;
     box-shadow: 0px 1px 5px rgba(0,0,0,0.1);
+    position: fixed;
+    top: 18.75vh;
+    right: 2.5%;
 `

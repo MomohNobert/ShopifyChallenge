79c9c36bed8a5303d746c3c9a50eb4f936a7b086
MomohNobert <okhai0112@gmail.com>
2021-01-17 14:59:39 +0100

    added nominations reducer to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import moviesReducer from '../app/components/Search/search.slice';
+import nominationsReducer from '../app/components/MovieCards/movieCards.slice';
 
 export default configureStore({
   reducer: {
-    movies: moviesReducer
+    movies: moviesReducer,
+    nominations: nominationsReducer
   },
 });

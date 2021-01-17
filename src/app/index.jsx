5f6c6ffbd9ee503705a93dcfad5342d8c4bbd579
MomohNobert <okhai0112@gmail.com>
2021-01-17 16:41:17 +0100

    to be more specific

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,5 +1,5 @@
 import { ContentContainer, GlobalStyle } from './index.styles';
-import Header from './components/Header/Header.component';
+import Header from './components/Header/Header.component.jsx';
 import Movies from './templates/Movies/Movies.template';
 import Nominations from './templates/Nominations/Nominations.template';
 

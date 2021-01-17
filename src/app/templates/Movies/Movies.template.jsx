3fe7d800a6b8766eee1386f1f141602ad5ef8f2b
MomohNobert <okhai0112@gmail.com>
2021-01-17 14:59:57 +0100

    cleaned up movies template

--- src/app/templates/Movies/Movies.template.jsx
+++ src/app/templates/Movies/Movies.template.jsx
@@ -1,15 +1,16 @@
 import { MoviesContainer } from "./Movies.styles";
 import { useSelector } from 'react-redux';
+import { moviesSelector } from "../../components/Search/search.slice";
+
 import EmptyMovies from "../../components/EmptyMovie/emptyMovie.component";
 import Loader from '../../components/Loader/loader.component';
 import MovieCards from "../../components/MovieCards/movieCards.component";
 import NoMovie from "../../components/NoMovie/noMovie.component";
 import ErrorMovie from "../../components/ErrorMovie/errorMovie.component";
 
+
 const Movies = () => {
-    const { loading, hasErrors, movies, searchValue } = useSelector(state => state.movies);
-    console.log(loading, hasErrors, movies, searchValue);
-    console.log(movies)
+    const { loading, hasErrors, movies, searchValue } = useSelector(moviesSelector);
 
     const currentComponent = () => {
         if (!searchValue) {

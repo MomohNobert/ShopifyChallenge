1bc69dd6909898e6c723b8b4b2c50f3f9fe3137b
MomohNobert <okhai0112@gmail.com>
2021-01-17 14:58:56 +0100

    added nominations reducer to store

--- src/app/templates/Nominations/Nominations.template.jsx
+++ src/app/templates/Nominations/Nominations.template.jsx
@@ -1,9 +1,22 @@
+import { useSelector } from "react-redux";
+import EmptyNom from "../../components/EmptyNominations/emptyNom.component";
+import NomCards from "../../components/NominationsCards/NomCards.component";
 import { NomContainer } from "./Nominations.styles"
 
 const Nominations = () => {
+    const { currentNominations, currentSize } = useSelector(state => state.nominations);
+
+    const currentComponent = () => {
+        if (currentSize <= 0) {
+            return <EmptyNom />
+        } else {
+            return <NomCards currentNominations={currentNominations} />
+        }
+    }
+
     return (
         <NomContainer>
-            Nominations
+            {currentComponent()}
         </NomContainer>
     )
 }

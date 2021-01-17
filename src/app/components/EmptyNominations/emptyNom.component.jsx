bda9daf9e9a5dbcc06ff1bbe48b6ff654d6f9ba1
MomohNobert <okhai0112@gmail.com>
2021-01-17 14:56:32 +0100

    created emptyNom component

---
+++ src/app/components/EmptyNominations/emptyNom.component.jsx
@@ -0,0 +1,12 @@
+import { ENContainer } from './emptyNom.styles';
+
+const EmptyNom = () => {
+    return (
+        <ENContainer>
+            <h1>No Nominations Available</h1>
+            <p>GO START NOMINATING!</p>
+        </ENContainer>
+    )
+}
+
+export default EmptyNom;

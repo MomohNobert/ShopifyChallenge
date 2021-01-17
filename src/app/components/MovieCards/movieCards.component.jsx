f466f3d2eafa16a8c877fda2ec53ccaa6998cf63
MomohNobert <okhai0112@gmail.com>
2021-01-17 14:55:52 +0100

    added nomination logic

--- src/app/components/MovieCards/movieCards.component.jsx
+++ src/app/components/MovieCards/movieCards.component.jsx
@@ -1,18 +1,45 @@
+import { useDispatch, useSelector } from 'react-redux';
 import MovieCard from "../../molecules/MovieCard/movieCard.molecule";
+import { nominationsSelector, addNomination } from './movieCards.slice'
 import { MCContainer, MCsContainer } from "./movieCards.styles";
 
 const MovieCards = ({ searchValue, movies }) => {
+    const dispatch =  useDispatch();
+    const { currentNominations, currentSize } = useSelector(nominationsSelector);
+
+    const disabledChecker = value => {
+        let result = false
+        if (currentSize >= 5) {
+            return result = true 
+        } 
+        currentNominations.map(({ imdbID }) => {
+            if (imdbID === value) {
+                return result = true
+            }
+            return result
+        })
+        return result
+    }
+
+
+    const clickHandler = value => {
+        dispatch(addNomination(value))
+    }
+
     return (
         <MCContainer>
             <h1>Showing results for "{searchValue}"</h1>
             <MCsContainer>
                 {
-                    movies.map(movie => (
+                    movies.map(movie => {
+                        return (
                         <MovieCard 
                             key={movie.imdbID} 
                             movie={movie} 
+                            clickHandler={clickHandler}
+                            disabled={disabledChecker(movie.imdbID)}
                         />
-                    ))
+                    )})
                 }
             </MCsContainer>
         </MCContainer>

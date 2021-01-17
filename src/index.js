48754bb3ad377d829ea80995c8d9ec98ae2262aa
MomohNobert <okhai0112@gmail.com>
2021-01-17 15:19:27 +0100

    added redux persist

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import store from './store/index';
 import App from './app/index';
 import { Provider } from 'react-redux';
 
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+
+let persistor = persistStore(store);
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor} >
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
 import moviesReducer from '../app/components/Search/search.slice';
 import nominationsReducer from '../app/components/MovieCards/movieCards.slice';
 
+const reducer = combineReducers({
+  movies: moviesReducer,
+  nominations: nominationsReducer
+})
+
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer)
+
 export default configureStore({
-  reducer: {
-    movies: moviesReducer,
-    nominations: nominationsReducer
-  },
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
 });

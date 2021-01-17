15aafa5ca779e9b57858e091370d0b3430ed79d3
MomohNobert <okhai0112@gmail.com>
2021-01-17 10:28:44 +0100

    added to movie template

--- src/app/templates/Movies/Movies.template.jsx
+++ src/app/templates/Movies/Movies.template.jsx
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux';
 import EmptyMovies from "../../components/EmptyMovie/emptyMovie.component";
 import Loader from '../../components/Loader/loader.component';
 import MovieCards from "../../components/MovieCards/movieCards.component";
+import NoMovie from "../../components/NoMovie/noMovie.component";
+import ErrorMovie from "../../components/ErrorMovie/errorMovie.component";
 
 const Movies = () => {
     const { loading, hasErrors, movies, searchValue } = useSelector(state => state.movies);
@@ -14,8 +16,12 @@ const Movies = () => {
             return <EmptyMovies />
         } else if (loading) {
             return <Loader />
+        } else if (typeof movies === "undefined") {
+            return <NoMovie searchValue={searchValue} />
         } else if (movies.length > 0) {
             return <MovieCards searchValue={searchValue} movies={movies} />
+        } else if (hasErrors) {
+            return <ErrorMovie />
         }
     }
 

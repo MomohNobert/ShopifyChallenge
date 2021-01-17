e3fd16668098540b96eab3a1a2a5abb9b9f864d5
MomohNobert <okhai0112@gmail.com>
2021-01-17 16:00:09 +0100

    fixed persist error from store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,16 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore, combineReducers, getDefaultMiddleware } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
-import { persistReducer } from 'redux-persist';
 import moviesReducer from '../app/components/Search/search.slice';
 import nominationsReducer from '../app/components/MovieCards/movieCards.slice';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 const reducer = combineReducers({
   movies: moviesReducer,
@@ -19,4 +27,10 @@ const persistedReducer = persistReducer(persistConfig, reducer)
 export default configureStore({
   reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== 'production',
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+
 });
